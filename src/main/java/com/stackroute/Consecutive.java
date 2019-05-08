a834315a8f97d9fae02bd818026677bee2e4a800
Bhavya <bhavya.desai23@gmail.com>
2019-05-08 09:43:24 +0530

    modified

---
+++ src/main/java/com/stackroute/Consecutive.java
@@ -0,0 +1,78 @@
+package com.stackroute;
+
+public class Consecutive {
+    boolean areConsecutive(int arr[], int n)
+    {
+        if (n < 1)
+            return false;
+
+        /* 1) Get the minimum element in array */
+        int min = getMin(arr, n);
+
+        /* 2) Get the maximum element in array */
+        int max = getMax(arr, n);
+
+        /* 3) max-min+1 is equal to n then only check all elements */
+        if (max - min + 1 == n)
+        {
+            int i;
+            for (i = 0; i < n; i++)
+            {
+                int j;
+
+                if (arr[i] < 0)
+                    j = -arr[i] - min;
+                else
+                    j = arr[i] - min;
+
+                // if the value at index j is negative then
+                // there is repitition
+                if (arr[j] > 0)
+                    arr[j] = -arr[j];
+                else
+                    return false;
+            }
+
+           /* If we do not see a negative value then all elements
+              are distinct */
+            return true;
+        }
+
+        return false; // if (max-min+1 != n)
+    }
+
+    /* UTILITY FUNCTIONS */
+    int getMin(int arr[], int n)
+    {
+        int min = arr[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] < min)
+                min = arr[i];
+        }
+        return min;
+    }
+
+    int getMax(int arr[], int n)
+    {
+        int max = arr[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+        return max;
+    }
+
+    /* Driver program to test above functions */
+    public static void main(String[] args)
+    {
+        Consecutive consecutive = new Consecutive();
+        int arr[] = {19,20,21,22,23,24,25};
+        int n = arr.length;
+        if (consecutive.areConsecutive(arr, n) == true)
+            System.out.println("Array elements are consecutive");
+        else
+            System.out.println("Array elements are not consecutive");
+    }
+}
---
+++ src/main/java/com/stackroute/TryCatch.java
@@ -0,0 +1,33 @@
+package com.stackroute;
+
+import java.lang.*;
+import java.io.FileNotFoundException;
+
+    public class TryCatch {
+
+        TryCatch(String msg) {
+
+            msg = "This is bound to execute";
+            System.out.println(msg);
+
+        }
+
+        public static void main(String[] args) throws Exception {
+            try {
+
+                throw new FileNotFoundException();
+            }
+            catch (FileNotFoundException e) {
+                throw new Exception("File not found");
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+
+                System.out.println("Finally block executed");
+            }
+
+        }
+    }
+
+
